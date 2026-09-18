104dc44e4a7d9f4ce9c8a12ac25e8e89ab79e799
imbananacrazy <bananacrazyness21@gmail.com>
2026-09-17 21:14:03 -0600

    some text fixes

--- app/components/discord-clone-box.tsx
+++ app/components/discord-clone-box.tsx
@@ -6,7 +6,7 @@ export default function DiscordCloneBox() {
       </div>
       <div className="w-full bg-linear-to-br from-[#161b33]/30 to-[#0d0c1d]/70 p-2 rounded-b-sm">
         <div className="flex flex-col gap-1">
-          <h1 className="text-lg font-bold">Discord Clone</h1>
+          <h1 className="text-lg font-bold">Real-time Chat App</h1>
           <p className="text-sm">
             A sloppy Discord clone made in Next.js purely for fun to see if I
             could do it.
